137f191900b74c09357991fc4d1ba69536d7b73d
JulieSemenova <julie.loggi@gmail.com>
2019-05-06 16:47:44 +0300

    ✨add action and mutation

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -18,8 +18,12 @@ export default {
   },
   created() {
     EventService.getEvents()
-      .then(response => (this.events = response.data))
-      .catch(error => console.log(error.response));
+      .then(response => {
+        this.events = response.data;
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
   }
 };
 </script>
