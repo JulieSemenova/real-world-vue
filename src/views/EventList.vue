5c417bdefb3482b2c0385e2d53a89b241db473d9
JulieSemenova <julie.loggi@gmail.com>
2019-05-13 15:47:40 +0300

    ✨ add pagination, single fetch event

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,29 +1,40 @@
 <template>
   <div>
     <h1>Events listing</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <template v-if="page != 1">
+      <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev">Prev Page</router-link>
+    </template>
+    <template v-if="hasNextPage">|</template>
+    <template v-if="hasNextPage">
+      <router-link :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next">Next Page</router-link>
+    </template>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import EventCard from '@/components/EventCard.vue';
-import EventService from '@/services/EventService.js';
 
 export default {
   components: {
     EventCard
   },
-  data() {
-    return { events: [] };
-  },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data;
-      })
-      .catch(error => {
-        console.log(error.response);
-      });
+    this.perPage = 3;
+    this.$store.dispatch('fetchEvents', {
+      perPage: this.perPage,
+      page: this.page
+    });
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1;
+    },
+    hasNextPage() {
+      return this.eventsTotal > this.page * this.perPage;
+    },
+    ...mapState(['events', 'eventsTotal'])
   }
 };
 </script>
